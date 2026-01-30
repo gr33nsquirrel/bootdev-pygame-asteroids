ae43636c90fec3c3a9c1dc5883c1525c34ae83ec
gr33nsquirrel <gr33n.squirr3l@gmail.com>
2026-01-30 12:58:26 +0000

    02 - add screen settings

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from constants.py import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(SCREEN_WIDTH)
+    print(SCREEN_HEIGHT)
 
 
 if __name__ == "__main__":
