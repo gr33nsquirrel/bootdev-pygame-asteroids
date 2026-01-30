0ce63ba1ef114378456f2c5f7afda7ba3f8d3d28
gr33nsquirrel <gr33n.squirr3l@gmail.com>
2026-01-30 14:09:40 +0000

    04. working with module import

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"{SCREEN_WIDTH}")
-    print(f"{SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
