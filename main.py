ec4a7ab0f0db9f3f5a6e5b656db4548951f6460a
gr33nsquirrel <gr33n.squirr3l@gmail.com>
2026-01-31 10:40:07 +0000

    07. added quit code, added FPS

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         log_state()
+        
         for event in pygame.event.get():
-            clock.tick(60)
-            dt = clock.tick(60) / 1000
-    screen.fill("black")
-    pygame.display.flip()
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
