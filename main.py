9c7cfb397d2149f684d5e5386dda4d692405d549
gr33nsquirrel <gr33n.squirr3l@gmail.com>
2026-02-09 16:07:51 +0000

    added object containers

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
@@ -23,8 +28,9 @@ def main():
                 return
         
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
